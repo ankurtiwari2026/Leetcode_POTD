882018874dcf661809b0b4bc6c8d4cffb6b15c17
ankur <2k22.csiot.2212422@gmail.com>
2024-03-05 12:09:01 +0530

    Solution using two Pointers

---
+++ leetcodePOTD/05-march-2024.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        #taking two pointers to track the length of string
+        left, right = 0, len(s) - 1
+        
+        while left < right and s[left] == s[right]:
+            char = s[left]
+            while left <= right and s[left] == char:
+                left += 1
+            while right >= left and s[right] == char:
+                right -= 1
+        
+        return right - left + 1
