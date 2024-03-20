6fba05c2569ec939178e6ccbee4f0721cd63006e
ankur <2k22.csiot.2212422@gmail.com>
2024-03-20 23:46:23 +0530

    1669. Merge In Between Linked Lists:

---
+++ leetcodePOTD/20-march-2024.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        ptr = list1
+        for _ in range(a - 1):
+            ptr = ptr.next
+        
+        qtr = ptr.next
+        for _ in range(b - a + 1):
+            qtr = qtr.next
+        
+        ptr.next = list2
+        while list2.next:
+            list2 = list2.next
+        list2.next = qtr
+        
+        return list1     
